f83c46487e0642ecbdbc9f83c0e56255cd9766af
Danial Dervovic <dannydervovic@hotmail.com>
2017-01-20 01:29:06 +0000

    Issue #5: CMake builds after replacement. TODO: namespace (something clever) and README

--- rename.sh
+++ rename.sh
@@ -8,7 +8,9 @@
 ##                 EDIT THIS PART                   ##
 ######################################################
 
-NEW_PROJECT_NAME='NewProject';
+NEW_PROJECT_NAME_CAMEL_CASE='NewProject';
+NEW_PROJECT_NAME_LOWER_CASE='newproject';
+NEW_PROJECT_NAME_CAPS='NEWPROJECT';
 NEW_SHORT_DESCRIPTION='A project for doing stuff.';
 NEW_NAMESPACE='newproj';
 
@@ -18,7 +20,10 @@ NEW_NAMESPACE='newproj';
 # Strings to replace
 
 OLD_DIR_NAME='CMakeCatchTemplate';
-OLD_PROJECT_NAME='MyProject';
+OLD_PROJECT_NAME_CAMEL_CASE='MyProject';
+OLD_PROJECT_NAME_LOWER_CASE='myproject';
+OLD_PROJECT_NAME_CAPS='MYPROJECT';
+OLD_DOXYGEN_INTRO='MyProject is a software library to perform whatever.'
 OLD_SHORT_DESCRIPTION='MyProject: A software package for whatever.';
 OLD_NAMESPACE='mp';
 
@@ -29,11 +34,23 @@ find_and_replace_string(){
 }
 
 # Change comment at top of each file describing project
-find_and_replace_string "${OLD_SHORT_DESCRIPTION}" "${NEW_PROJECT_NAME}"": ""${NEW_SHORT_DESCRIPTION}" ;
+find_and_replace_string "${OLD_SHORT_DESCRIPTION}" "${NEW_PROJECT_NAME_CAMEL_CASE}"": ""${NEW_SHORT_DESCRIPTION}" ;
 
 # Change Doxygen intro
-find_and_replace_string "MyProject is a software library to perform whatever." "${NEW_PROJECT_NAME}"": ""${NEW_SHORT_DESCRIPTION}";
+find_and_replace_string "${OLD_DOXYGEN_INTRO}" "${NEW_PROJECT_NAME_CAMEL_CASE}"": ""${NEW_SHORT_DESCRIPTION}";
 
-# Replace name MyProject
-#find_and_replace_string "MyProject" "$NEW_PROJECT_NAME"
+# Replace name MyProject, myproject, MYPROJECT
+find_and_replace_string "$OLD_PROJECT_NAME_CAMEL_CASE" "$NEW_PROJECT_NAME_CAMEL_CASE"
+find_and_replace_string "$OLD_PROJECT_NAME_LOWER_CASE" "$NEW_PROJECT_NAME_LOWER_CASE"
+find_and_replace_string "$OLD_PROJECT_NAME_CAPS" "$NEW_PROJECT_NAME_CAPS"
+
+# namespace
+## Need to do summat clever here
+#find_and_replace_string "$OLD_NAMESPACE" "$NEW_NAMESPACE"
+
+# Filename replacements
+find . -name *${OLD_PROJECT_NAME_CAMEL_CASE}* | sed -e "p;s/${OLD_PROJECT_NAME_CAMEL_CASE}/${NEW_PROJECT_NAME_CAMEL_CASE}/" | xargs -n2 mv
+find . -name *${OLD_PROJECT_NAME_LOWER_CASE}* | sed -e "p;s/${OLD_PROJECT_NAME_LOWER_CASE}/${NEW_PROJECT_NAME_LOWER_CASE}/" | xargs -n2 mv
+find . -name *${OLD_PROJECT_NAME_CAPS}* | sed -e "p;s/${OLD_PROJECT_NAME_CAPS}/${NEW_PROJECT_NAME_CAPS}/" | xargs -n2 mv
+
 #mv ../CMakeCatchTemplate ../"${NEW_PROJECT_NAME}"
