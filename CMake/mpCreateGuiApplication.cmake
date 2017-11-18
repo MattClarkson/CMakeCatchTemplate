1ac8f98efe4f50168670681b837c6ee0bec669e3
Matt Clarkson <m.clarkson@ucl.ac.uk>
2017-11-18 19:58:05 +0000

    Issue #12: Re-instate fixup_bundle.

--- CMake/mpCreateGuiApplication.cmake
+++ CMake/mpCreateGuiApplication.cmake
@@ -64,14 +64,14 @@ macro(mpCreateGuiApplication APP_NAME ADDITIONAL_SEARCH_PATHS)
     endif()
   endif()
 
-#  install(CODE "
-#          file(GLOB_RECURSE QTPLUGINS
-#          \"\${CMAKE_INSTALL_PREFIX}/${plugin_dest_dir}/platforms/*${CMAKE_SHARED_LIBRARY_SUFFIX}\")
-#          file(GLOB_RECURSE QMLPLUGINS
-#          \"\${CMAKE_INSTALL_PREFIX}/${plugin_dest_dir}/qml/*${CMAKE_SHARED_LIBRARY_SUFFIX}\")
-#          list(APPEND QTPLUGINS \"\${QMLPLUGINS}\")
-#          include(BundleUtilities)
-#          fixup_bundle(\"${APPS}\" \"\${QTPLUGINS}\" \"${ADDITIONAL_SEARCH_PATHS}\")
-#          " COMPONENT Runtime)
+  install(CODE "
+          file(GLOB_RECURSE QTPLUGINS
+          \"\${CMAKE_INSTALL_PREFIX}/${plugin_dest_dir}/platforms/*${CMAKE_SHARED_LIBRARY_SUFFIX}\")
+          file(GLOB_RECURSE QMLPLUGINS
+          \"\${CMAKE_INSTALL_PREFIX}/${plugin_dest_dir}/qml/*${CMAKE_SHARED_LIBRARY_SUFFIX}\")
+          list(APPEND QTPLUGINS \"\${QMLPLUGINS}\")
+          include(BundleUtilities)
+          fixup_bundle(\"${APPS}\" \"\${QTPLUGINS}\" \"${ADDITIONAL_SEARCH_PATHS}\")
+          " COMPONENT Runtime)
 
 endmacro()
