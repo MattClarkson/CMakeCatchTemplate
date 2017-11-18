a8eec74827476f67ed7431414f58c1c4fa5193c5
Matt Clarkson <m.clarkson@ucl.ac.uk>
2017-11-18 21:42:40 +0000

    Issue #12: Add MYPROJECTConfig and UseMYPROJECT to enable 3rd parties to link to us.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,11 +1,11 @@
 #/*============================================================================
 #
-#  USCal: A software package for ultrasound calibration.
+#  MYPROJECT: A software package for whatever.
 #
 #  Copyright (c) University College London (UCL). All rights reserved.
 #
 #  This software is distributed WITHOUT ANY WARRANTY; without even
-#  the iusclied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 #  PURPOSE.
 #
 #  See LICENSE.txt in the top level directory for details.
