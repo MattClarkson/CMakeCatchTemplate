91f84e658f07ac6e1d4af22c1e183d866a2eef7e
Matt Clarkson <m.clarkson@ucl.ac.uk>
2018-01-25 06:28:24 +0000

    Issue #27: Change mpSetupDoxygen.cmake to mpSetupDocs.cmake

--- CMake/mpSetupDocs.cmake
+++ CMake/mpSetupDocs.cmake
@@ -17,7 +17,7 @@ if(BUILD_Docs)
   find_package(Doxygen REQUIRED)
   configure_file(${CMAKE_SOURCE_DIR}/Utilities/Doxygen/myprojectdoxygen.pl.in ${CMAKE_BINARY_DIR}/myprojectdoxygen.pl)
   configure_file(${CMAKE_SOURCE_DIR}/Utilities/Doxygen/doxygen.config.in ${CMAKE_BINARY_DIR}/doxygen.config)
-  add_custom_target(doc ALL
+  add_custom_target(docs ALL
     ${DOXYGEN_EXECUTABLE} ${CMAKE_BINARY_DIR}/doxygen.config
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     COMMENT "Generating API documentation with Doxygen" VERBATIM
