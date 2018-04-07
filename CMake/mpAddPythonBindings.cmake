229fd6d9236f69f7287f9ce32cbf17bfae29d051
Matt Clarkson <m.clarkson@ucl.ac.uk>
2018-04-07 10:57:41 +0100

    Issue #37: Try to cope if PythonLibs not found.

--- CMake/mpAddPythonBindings.cmake
+++ CMake/mpAddPythonBindings.cmake
@@ -20,15 +20,23 @@ if(BUILD_Python_Boost AND BUILD_Python_PyBind)
 endif()
 
 if(BUILD_Python_Boost OR BUILD_Python_PyBind)
-
-  find_package(PythonInterp REQUIRED)
+  find_package(PythonInterp)
   message("Found python interpreter: ${PYTHON_EXECUTABLE}")
-  find_package(PythonLibs REQUIRED)
+  find_package(PythonLibs)
   message("Found python library version: ${PYTHONLIBS_VERSION_STRING}")
+endif()
 
-  set(BUILD_SHARED_LIBS OFF CACHE BOOL "Build Shared Libraries" FORCE)
-  message("Forcing BUILD_SHARED_LIBS to OFF as you want a python module.")
+if (NOT PythonLibs_FOUND)
+  set(BUILD_Python_Boost OFF CACHE BOOL "Build boost::python bindings." FORCE)
+  set(BUILD_Python_PyBind OFF CACHE BOOL "Build PyBind11 bindings." FORCE)
+  message("Forcing BUILD_Python_Boost and BUILD_Python_PyBind to OFF as no Python libs were found.")
+endif()
 
+if (PythonLibs_FOUND)
+  if(BUILD_Python_Boost OR BUILD_Python_PyBind)
+    set(BUILD_SHARED_LIBS OFF CACHE BOOL "Build Shared Libraries" FORCE)
+    message("Forcing BUILD_SHARED_LIBS to OFF as you want a python module.")
+  endif()
 endif()
 
 if(BUILD_Python_Boost)
