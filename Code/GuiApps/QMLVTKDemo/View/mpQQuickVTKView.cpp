46ebf33ac14fdf5c9f475cfaea93b6771ced0cd0
Matt Clarkson <m.clarkson@ucl.ac.uk>
2018-07-11 20:15:44 +0100

    Revert "Issue #39: Reduce requirement for ReadyForRendering, as interactor should always update position."
    This reverts commit 88b8d3f30f5f85f873c552cf396b08d5fd4cd3b7.

--- Code/GuiApps/QMLVTKDemo/View/mpQQuickVTKView.cpp
+++ Code/GuiApps/QMLVTKDemo/View/mpQQuickVTKView.cpp
@@ -27,9 +27,7 @@ QQuickVTKView::~QQuickVTKView()
   {
     m_VTKRenderWindowInteractor->Disable();
     m_VTKRenderWindow->SetInteractor(nullptr);
-#ifdef BUILD_VTK_OpenGL2
     m_VTKRenderWindow->SetReadyForRendering(false);
-#endif
     m_VTKRenderWindow->SetMapped(0);
     m_VTKRenderWindow->RemoveAllObservers();
     vtkRendererCollection *c = m_VTKRenderWindow->GetRenderers();
@@ -159,7 +157,7 @@ bool QQuickVTKView::event(QEvent *e)
     )
   {
     QMutexLocker lock(&m_Mutex);
-    if(m_VTKRenderWindow)
+    if(m_VTKRenderWindow && m_VTKRenderWindow->GetReadyForRendering())
     {
       m_VTKInteractorAdapter->ProcessEvent(e, m_VTKRenderWindow->GetInteractor());
       if (e->isAccepted())
@@ -176,7 +174,7 @@ bool QQuickVTKView::event(QEvent *e)
 bool QQuickVTKView::ProcessEvent(QEvent* e)
 {
   QMutexLocker lock(&m_Mutex);
-  if (m_VTKRenderWindow)
+  if (m_VTKRenderWindow && m_VTKRenderWindow->GetReadyForRendering())
   {
     return m_VTKInteractorAdapter->ProcessEvent(e, m_VTKRenderWindowInteractor);
   }
