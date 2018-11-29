0be6c848115ba3dfd7bd67edd7b9be37628bbd29
Matt Clarkson <m.clarkson@ucl.ac.uk>
2018-11-29 20:54:48 +0000

    Issue #43: Illustrate where to put numpy if you want Boost.Numpy.

--- CMake/mpAddPythonBindings.cmake
+++ CMake/mpAddPythonBindings.cmake
@@ -41,6 +41,11 @@ if(BUILD_Python_Boost OR BUILD_Python_PyBind)
     list(APPEND MYPROJECT_BOOST_LIBS "system")
     if (WITHIN_SUBBUILD)
       list(APPEND MYPROJECT_BOOST_LIBS "python${PYTHON_VERSION_MAJOR}${PYTHON_VERSION_MINOR}")
+	  
+	  # Add this if you want numpy. Depends on your build env.
+	  # I've left it out for now, as cloned projects can decide if they need it.
+	  # It doesn't warrant another top level flag.
+	  # list(APPEND MYPROJECT_BOOST_LIBS "numpy${PYTHON_VERSION_MAJOR}${PYTHON_VERSION_MINOR}")
     else()
       list(APPEND MYPROJECT_BOOST_LIBS "python")
     endif()
