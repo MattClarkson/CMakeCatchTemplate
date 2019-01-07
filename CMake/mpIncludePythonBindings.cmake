722b0f658711bb298a8f5504f5f7249a666bc4c8
Matt Clarkson <m.clarkson@ucl.ac.uk>
2019-01-07 07:42:37 +0000

    Issue #44: Don't refer to setup.py as its unnecessary waffle.

--- CMake/mpIncludePythonBindings.cmake
+++ CMake/mpIncludePythonBindings.cmake
@@ -12,8 +12,6 @@
 #
 #============================================================================*/
 
-# If running pip install, MYPROJECT_PYTHON_OUTPUT_DIRECTORY is passed in from setup.py
-# So, if its not specified, we just put python library in same output folder as all other binaries.
 if(NOT MYPROJECT_PYTHON_OUTPUT_DIRECTORY OR MYPROJECT_PYTHON_OUTPUT_DIRECTORY STREQUAL "")
   set(MYPROJECT_PYTHON_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
 endif()
