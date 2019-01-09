a9033b80f7b51c257f573d349f761df461f2b4b4
Matt Clarkson <m.clarkson@ucl.ac.uk>
2019-01-09 09:42:01 +0000

    Issue #44: Switch to Matthew Brett's multibuild to get Mac OSX and Linux builds on Travis, using Docker.

---
+++ config.sh
@@ -0,0 +1,28 @@
+#/*============================================================================
+#
+#  MYPROJECT: A software package for whatever.
+#
+#  Copyright (c) University College London (UCL). All rights reserved.
+#
+#  This software is distributed WITHOUT ANY WARRANTY; without even
+#  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#  PURPOSE.
+#
+#  See LICENSE.txt in the top level directory for details.
+#
+#============================================================================*/
+
+# Define custom utilities
+# Test for macOS with [ -n "$IS_OSX" ]
+
+function pre_build {
+  # Any stuff that you need to do before you start building the wheels
+  # Runs in the root directory of this repository.
+  :
+}
+
+function run_tests {
+  # Runs tests on installed distribution from an empty directory
+  :
+}
+
