32a2dfc03bf8e468d8d49b64ff52b20a81a8c7c2
Matt Clarkson <m.clarkson@ucl.ac.uk>
2019-01-09 20:55:20 +0000

    Issue #44: First pass, setting Linux/Mac CMake build under multibuild.

--- config.sh
+++ config.sh
@@ -18,7 +18,13 @@
 function pre_build {
   # Any stuff that you need to do before you start building the wheels
   # Runs in the root directory of this repository.
-  :
+  pwd
+  python --version
+  cmake --version
+  cmake -DBUILD_SUPERBUILD:BOOL=ON -DBUILD_TESTING:BOOL=ON -DBUILD_Boost:BOOL=ON -DBUILD_Python_Boost:BOOL=ON -DBUILD_Eigen:BOOL=OFF -DBUILD_glog:BOOL=OFF -DBUILD_gflags:BOOL=OFF -DBUILD_VTK:BOOL=OFF -DBUILD_PCL:BOOL=OFF -DBUILD_OpenCV:BOOL=OFF ..
+  make
+  cd MYPROJECT-build
+  ctest .
 }
 
 function run_tests {
