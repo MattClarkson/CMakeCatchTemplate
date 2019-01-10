1e4373c09dae769142a4acde71ec8bda52ea7341
Matt Clarkson <m.clarkson@ucl.ac.uk>
2019-01-10 09:30:12 +0000

    Issue #44: For Linux/Mac builds on Travis, move DO_PYTHON_BUILD to env_vars.sh

---
+++ env_vars.sh
@@ -0,0 +1,17 @@
+#/*============================================================================
+#
+#  MYPROJECT: A software package for whatever.
+#
+#  Copyright (c) University College London (UCL). All rights reserved.
+#
+#  This software is distributed WITHOUT ANY WARRANTY; without even
+#  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#  PURPOSE.
+#
+#  See LICENSE.txt in the top level directory for details.
+#
+#============================================================================*/
+
+# This file is sourced for the Linux/Mac CI builds on Travis.
+# Set DO_PYTHON_BUILD to false, if you are only interested in the C++ build.
+DO_PYTHON_BUILD="true"
