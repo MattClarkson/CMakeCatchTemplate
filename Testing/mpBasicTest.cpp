3db1a613c11b6880368ab385a0b2c12b8dec60d0
tdowrick <tdowrick@gmail.com>
2019-11-29 16:57:09 +0000

    Update mpBasicTest.cpp

--- Testing/mpBasicTest.cpp
+++ Testing/mpBasicTest.cpp
@@ -81,6 +81,6 @@ TEST_CASE("OpenCV test", "[OpenCV]")
 {
 #ifdef BUILD_OpenCV
   cv::Matx44d mat = cv::Matx44d::eye();
-  REQUIRE(cv::determinant(mat) == 1)
+  REQUIRE(cv::determinant(mat) == 1);
 #endif
 }
